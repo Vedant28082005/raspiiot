d588e253521914ff177d1a2934232664f6936274
Viral Vaghela <viralvaghela8577@gmail.com>
2024-10-11 18:43:50 +0530

    add -dotenv

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
+require('dotenv').config()
 
-const mongoDBUrl = "mongodb+srv://vedant:vedant123@cluster0.x2uku.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const mongoDBUrl = process.env.MONGO_URI;
 
 const connectDb = async () => {
     try {
         await mongoose.connect(mongoDBUrl);
         console.log("Database connection successful 🎊");
-        
+
     } catch (error) {
         console.log('Error connecting to the database');
     }
 }
 
-module.exports = connectDb
+module.exports = connectDb;
